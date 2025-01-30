f4881697862916076cbe74925821b355359e9074
kishan7890 <kishansahu9348@gmail.com>
2025-01-30 15:47:17 +0530

    cors added

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const {connectDB} = require("./db/db");
 const userRouter= require("./routes/userRoutes")
 const noteRouter=require("./routes/noteRoutes");
-
+const cors = require("cors");
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 
 
